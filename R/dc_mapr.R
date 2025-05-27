748d985e1fa5bb4da03f20830198b00ac4ff2b75
christianmainovieytes <cam17@illinois.edu>
2025-05-27 16:02:16 -0400

    finish merging section
    merging of shapefile to attribute data for ward

--- R/dc_mapr.R
+++ R/dc_mapr.R
@@ -10,7 +10,7 @@
 #' @details
 #' Attribute data in `d` (see below) should be organized by ward, ZIP-code tabulation area (ZCTA), census tract, or block. 
 #' @param d A data frame or tibble with the attribute data used to fill the shapes.
-#' @param geo One of "ward", "zcta", "tract', or "block". The geography that is desired.
+#' @param geo One of "ward 2022", "ward 2012", "zcta", "tract', or "block". The geography that is desired.
 #' @param id Column name in `d` that contains the ward, census block, or ZCTA identifier.
 #' @param var Column name in `d` that contains the variable to be plotted.
 #' @param bypass A logical. If data supplied in `d` is already in aggregate form ready for mapping, then the function will only map the values supplied. Default is FALSE.
@@ -67,6 +67,10 @@ dc_mapr <- function( d, geo, id, bypass = FALSE,
   
   if( sum( metric %in% c( "percent", "rate", "count" ) ) != length( metric ) ){
     stop( '`metric` can only be one of the following exact strings: "percent", "rate", "count"' )
+  }
+  
+  if( length( geo ) != 1 | !geo %in% c( "ward 2022", "ward 2012", "zcta", "tract", "block" ) ){
+    stop( '`geo` can only be one of the following exact strings: "ward 2022", "ward 2012", "zcta", "tract", "block"' )
   }
   
   
@@ -170,9 +174,97 @@ dc_mapr <- function( d, geo, id, bypass = FALSE,
       if( sum( is.na( d.agg$denom ) > 0 ) ){ warning( paste0( "NAs detected for denominator in at least one level of `", 
                                                              id, "`" ) )
       }
+    
+    message( "...Data aggregation DONE" )
       
-
-    }
   }
   
+  ### (2.0) Now Map ###
   
+  message( "Calling shapefiles and merging attributes data..." )
+  
+  ## call shapefile based on `geo` specification ##
+  d.geo <- if( geo == "ward 2022" ){ 
+    message( "...Ward (2022 redistricting) shapfiles loaded..." )
+    dc.ward22
+    
+  } else if( geo == "ward 2012" ){
+    message( "...Ward (2012 redistricting) shapfiles loaded..." )
+    dc.ward12
+  }
+  
+  ## merge shapes with attributes data in `d.aggr` ##
+  
+  # first if wards are desired
+  if( geo %in% c( "ward 2022", "ward 2012" ) ){
+    
+    # first possible pattern
+    ward.pattern1 <- str_detect( levels( as.factor( d.aggr[[ id ]] ) ), 
+                "^ward\\s\\d+$|^Ward\\s\\d+$" )
+    
+    if( all( ward.pattern1 ) ){ 
+      message( paste0( "...'Ward \\d' naming pattern detected in `", 
+                       id, "` variable..." ) )
+      
+      d.aggr <- d.aggr %>% 
+        mutate( !!sym( id ) := firstup( !!sym( id ) ) ) # ensure upper case
+    }
+    
+    # second possible pattern
+    ward.pattern2 <- str_detect( levels( as.factor( d.aggr[[ id ]] ) ), 
+                                "^\\d+$" )
+    
+    if( all( ward.pattern2 ) ){ 
+      message( paste0( "...'\\d' naming pattern detected in `", 
+                                               id, "` variable for ward..." ) )
+      
+      d.aggr <- d.aggr %>% 
+        mutate( !!sym( id ) := str_remove_all( !!sym( id ), "\\s+" ),
+                !!sym( id ) := paste0( "Ward ", !!sym( id ) ) )
+    
+    }
+    
+    # third possible pattern (spelled out integers)
+    ward.pattern3 <- all( levels( as.factor( d.aggr[[ id ]] ) ) %in% 
+                            as.character( english::as.english( 1:8 ) ) |
+                            levels( as.factor( d.aggr[[ id ]] ) ) %in% 
+                            firstup( as.character( english::as.english( 1:8 ) ) ) )
+    
+    if( all( ward.pattern3 ) ){ 
+      message( paste0( "...'Spelled integers' naming pattern detected in `", 
+                       id, "` variable for ward..." ) )
+      
+      d.aggr$id <- vapply( d.aggr$id, FUN = function(x){
+        
+        switch( x, # assign mean ages for each ward
+                         "One" = "Ward 1", "one" = "Ward 1",
+                         "Two" = "Ward 2", "two" = "Ward 2",
+                         "Three" = "Ward 3", "three" = "Ward 3",
+                         "Four" = "Ward 4", "four" = "Ward 4",
+                         "Five" = "Ward 5", "five" = "Ward 5",
+                         "Six" = "Ward 6", "six" = "Ward 6",
+                         "Seven" = "Ward 7", "seven" = "Ward 7",
+                         "Eight" = "Ward 8", "eight" = "Ward 8" )
+        
+      }, FUN.VALUE = c("char"), 
+      USE.NAMES = FALSE ) 
+      
+    }
+    
+    if( all( !c( all( ward.pattern1 ), 
+                 all( ward.pattern2 ), 
+                 all( ward.pattern3 ) ) ) ){
+      stop( "Levels of ward in `id` are in an unrecognizable format. Recommend using 'Ward \\d` naming." )
+      
+    }
+    
+    message( "Merging attributes (i.e., `var`) to shapefile for ward." )
+    
+    # final merge
+    d.map <- left_join( d.geo, d.aggr,
+                        by = c( "NAMELSAD" = id ) )
+    
+  }
+}
+  
+message( "Attribute and shapefile merging...DONE" )
